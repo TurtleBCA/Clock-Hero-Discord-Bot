93a89bc34eed1369129b36c6afa02b49bba38b82
Elias Cho <33488019+TurtleBCA@users.noreply.github.com>
2021-04-22 17:39:16 -0400

    ping command

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ require('dotenv').config()
 
 const Discord = require('discord.js')
 
-const client = new Discord.client();
+const client = new Discord.Client()
+const prefix = '!'
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        message.channel.send('pinged');
+    }
+})
 
 client.login(process.env.BOT_TOKEN)
