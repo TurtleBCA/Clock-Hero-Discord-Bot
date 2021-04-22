82f57903e9a48e93d4a8fa6a81cacc0aa94b1064
Elias Cho <33488019+TurtleBCA@users.noreply.github.com>
2021-04-22 17:53:14 -0400

    new command architecture

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 
-const client = new Discord.Client()
-const prefix = '!'
+const client = new Discord.Client();
+const prefix = '!';
+const fs = require('fs');
+
+client.commands = new Discord.Collection()
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+const commandSet = new Set()
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+    commandSet.add(command.name);
+}
 
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if (command === 'ping') {
-        message.channel.send('pinged');
+    if (commandSet.has(command)) {
+        client.commands.get(command).execute(message, args);
     }
-})
+});
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
