ed820c99a16eaac74a72b114fbe1d9de65967029
Elias Cho <33488019+TurtleBCA@users.noreply.github.com>
2021-04-22 18:58:29 -0400

    hero info command

--- commands/hero.js
+++ commands/hero.js
@@ -17,6 +17,13 @@ module.exports = {
                 message.channel.send('creating one');
                 heroes[message.author.tag] = {name: args[1], currentHP: 20, totalExp: 0, level: 1, health: 1, attack: 1, defense: 1};
             }
+        } else if (subcommand === 'info') {
+            if (args.length) {
+                const hero = heroes[message.author.tag];
+                const maxHP = Math.pow(hero.health, Math.log(9981) / Math.log(100)) + 19;
+                let currentExp = hero.totalExp - ((hero.level - 1) * (20 + 20 + hero.level - 2) / 2);
+                message.channel.send(`${hero.name}: HP ${hero.currentHP}/${maxHP} \\|\\| Lv. ${hero.level} ${currentExp}/${20+hero.level-1} \\|\\| Stat ${hero.health}/${hero.attack}/${hero.defense}`);
+            }
         }
 
         fs.writeFile('heroes.json', JSON.stringify(heroes), (err) => {});
