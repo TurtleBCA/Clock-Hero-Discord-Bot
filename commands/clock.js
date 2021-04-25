3a035d0198b8d0a7220d4bf4ec172fe196040601
Elias Cho <33488019+TurtleBCA@users.noreply.github.com>
2021-04-25 17:51:00 -0400

    Change log and goal

--- commands/clock.js
+++ commands/clock.js
@@ -15,15 +15,27 @@ module.exports = {
             if (args.length == 1) {
                 message.channel.send(`Goal: ${clock[message.author.toString()].goal} minutes`);
             } else if (args.length == 2) {
-                clock[message.author.toString()].goal = args[1];
-                message.channel.send(`Goal has been set to ${clock[message.author.toString()].goal} minutes`);
+                const minutes = parseInt(args[1]);
+                if (args[1][0] == '+' || args[1][0] == '-') {
+                    message.channel.send(`Goal: ${clock[message.author.toString()].goal} -> ${Math.max(clock[message.author.toString()].goal + minutes, 0)}`);
+                    clock[message.author.toString()].goal = Math.max(clock[message.author.toString()].goal + minutes, 0);
+                } else {
+                    clock[message.author.toString()].goal = minutes;
+                    message.channel.send(`Goal has been set to ${clock[message.author.toString()].goal} minutes`);
+                }
             }
         } else if (subcommand === 'log') {
             if (args.length == 1) {
                 message.channel.send(`Log: ${clock[message.author.toString()].log} minutes`);
             } else if (args.length == 2) {
-                clock[message.author.toString()].log = args[1];
-                message.channel.send(`Log has been set to ${clock[message.author.toString()].log} minutes`);
+                const minutes = parseInt(args[1]);
+                if (args[1][0] == '+' || args[1][0] == '-') {
+                    message.channel.send(`Log: ${clock[message.author.toString()].log} -> ${Math.max(clock[message.author.toString()].log + minutes, 0)}`);
+                    clock[message.author.toString()].log = Math.max(clock[message.author.toString()].log + minutes, 0);
+                } else {
+                    clock[message.author.toString()].log = minutes;
+                    message.channel.send(`Log has been set to ${clock[message.author.toString()].log} minutes`);
+                }
             }
         }
 
